59e5c2e16ebd03811edccc599f347b7f59c89b76
Lucas Bocquet <159040687+lbocquet0@users.noreply.github.com>
2025-01-24 11:49:16 +0100

    fin du tp

--- TP1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ TP1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -10,7 +11,9 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+const val EXTRA_TEXT = "text_to_display"
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,11 +24,12 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        var text : String = ""
         val premierBouton : Button = findViewById(R.id.premierButton)
         premierBouton.setOnClickListener{
             val textView : TextView = findViewById(R.id.textView)
             val editText : EditText = findViewById(R.id.editTextText)
-            val text = editText.text.toString()
+            text = editText.text.toString()
             if (editText.text.isEmpty()) {
                 textView.text = "Hello World!"
             } else if (text == "afficher nouveau textView") {
@@ -35,6 +39,16 @@ class MainActivity : AppCompatActivity() {
                 layoutPrincipal.addView(deuxiemeTextView)
             } else {
                 textView.text = editText.text
+            }
+        }
+
+
+        val nextButton : Button = findViewById(R.id.nextButton)
+        nextButton.setOnClickListener{
+            if (text != "") {
+                val intent = Intent(this, MainActivity2::class.java)
+                intent.putExtra(EXTRA_TEXT, text)
+                startActivity(intent)
             }
         }
     }
--- TP1/readme.md
+++ TP1/readme.md
@@ -5,4 +5,13 @@ R4A11\TP1\app\src\main\res\layout\activity_main.xml
 La ligne android:text="Coucou, j'ai trouvé comment faire !" du TextView dans le fichier activity_main.xml
 
 # Question 3 : Qu'avez-vous modifié pour changer l'icone ?
-La ligne android:icon="@mipmap/ic_launcher" du manifest
+La ligne android:icon="@mipmap/ic_launcher" et android:roundIcon="" du manifest
+
+# Question 4 : Est-ce nécessaire de cliquer sur le bouton valider pour afficher le texte saisi sur la seconde activité ? Pourquoi ?
+Non, on ne doit pas cliquer sur le bouton valider pour afficher le texte saisi sur la seconde activité car il n'y a pas de verification préalable au changement d'activité
+
+# Question 5 : Le comportement de la question 4 vous semble-t-il normal ?
+Non, nous devrions vérifier que le texte est bien saisi avant de changer d'activité
+
+# Question 6 : Comment faire pour ne pas afficher le nouveau texte sur la deuxième activité tant que le bouton valider n'a pas été cliqué ?
+Faire l'attribution de la variable dans le bouton valider et vérifier si la variable est vide ou non avant de changer d'activité
