1b809a025480e50c2971fbf24b3f25c69e778512
Lucas Bocquet <159040687+lbocquet0@users.noreply.github.com>
2025-02-05 14:37:50 +0100

    add text field

--- TP2NavigationCompose/app/src/main/java/com/example/tp2navigationcompose/MainActivity.kt
+++ TP2NavigationCompose/app/src/main/java/com/example/tp2navigationcompose/MainActivity.kt
@@ -8,13 +8,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
@@ -82,10 +88,20 @@ fun FormScreen(navController: NavController) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        var name by remember { mutableStateOf("")}
         Text(text = "Page de formulaire",
             style = MaterialTheme.typography.titleMedium)
 
         Spacer(modifier = Modifier.height(24.dp))
+
+        TextField(
+            value = name,
+            onValueChange = { newText -> name = newText},
+            label = { Text("Entrez votre nom") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+        )
 
         Button(onClick = { navController.popBackStack() }) {
             Text(text = "Retour")
